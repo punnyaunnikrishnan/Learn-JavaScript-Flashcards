cd77947a511f5da2f55ef20a9784daf5abfc9274
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-08-16 19:15:28 +0530

    004Shout my name.js

---
+++ Learn JavaScript  Flashcards/004Shout my name.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} name
+ */
+function shoutMyName(name) {
+  return name.toUpperCase();
+}
+console.log(shoutMyName("punnya"));
+// Sample usage - do not modify
+console.log(shoutMyName("Sam")); // "SAM"
+console.log(shoutMyName("Charley")); // "CHARLEY"
+console.log(shoutMyName("alex")); // "ALEX"
