d9f1e86936f4b03e129e29bbcc363055734ce8d9
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-08-16 19:15:45 +0530

    005Lower case string.js

---
+++ Learn JavaScript  Flashcards/005Lower case string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} name
+ */
+function lowerName(name) {
+  return name.toLowerCase();
+}
+console.log(lowerName("JISHNU"));
+// Sample usage - do not modify
+console.log(lowerName("Sam")); // "sam"
+console.log(lowerName("ALEX")); // "alex"
