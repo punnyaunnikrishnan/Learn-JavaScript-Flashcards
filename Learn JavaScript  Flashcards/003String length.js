606ecb9fa0afc1de9286d4c3c6d0ff9812ee2812
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-08-16 19:15:10 +0530

    003String length.js

---
+++ Learn JavaScript  Flashcards/003String length.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} str
+ */
+function getCharCount(str) {
+  return str.length;
+}
+console.log(getCharCount("12345"));
+
+// Sample usage - do not modify
+console.log(getCharCount("Sam")); // 3
+console.log(getCharCount("Alex 123")); // 8
+console.log(getCharCount("Charley is here")); // 15
