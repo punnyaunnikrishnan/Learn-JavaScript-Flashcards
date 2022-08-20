ea09697ff51d0119b990557f1f90854b7f9ef494
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-08-20 21:13:33 +0530

    008Skip first character.js

---
+++ Learn JavaScript  Flashcards/008Skip first character.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} text
+ */
+function skipFirstCharacter(text) {
+  return text.substring(1, 5);
+}
+
+// Sample usage - do not modify
+console.log(skipFirstCharacter("Xcode")); // "code"
+console.log(skipFirstCharacter("Hello")); // "ello"
