9219e239d56e918476fdeedf23200f5bb5231176
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-08-20 21:50:00 +0530

    014Multiline string.js

---
+++ Learn JavaScript  Flashcards/014Multiline string.js
@@ -0,0 +1,9 @@
+function getMultilineString() {
+  return `${getMultilineString}`;
+  getMultilineString = `I am learning JavaScript  
+    and I found it to be  
+    quite fun!`;
+}
+
+// Sample usage - do not modify
+console.log(getMultilineString());
